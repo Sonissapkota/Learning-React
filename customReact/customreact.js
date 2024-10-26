c70410d759f84712bb6b9756ddc7244a69e97609
sonissapkota <sonishapkota47@gmail.com>
2024-10-26 23:01:41 +0545

    Made the custom react file to accept multiple attributes

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,10 +1,20 @@
 function customRender(reactElement, container){
-    const domElement = document.createElement(reactElement.type)
-    domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
+    //The given techniques does not work in the element having single or more than two attribute
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
 
-    container.appendChild(domElement)
+    // container.appendChild(domElement)
+
+    //Making this function modular to handle one or more than two attribute at the same time
+    const docElement = document.createElement(reactElement.type)
+    docElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props){
+        docElement.setAttribute(prop, reactElement.props[prop])
+    }
+
+    container.appendChild(docElement)
 }
 
 const reactElement = {
