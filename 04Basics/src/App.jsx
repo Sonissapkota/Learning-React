490f16f6e69269885aa845b656adcca6c57ba120
Sonissapkota <sonishsapkota47@gmail.com>
2024-10-28 21:18:19 +0545

    Using callback to update the value of counter

--- 04Basics/src/App.jsx
+++ 04Basics/src/App.jsx
@@ -5,9 +5,17 @@ function App() {
   const [counter,setCounter] = useState(15)
 
   const addValue = () =>{
-    setCounter(counter+1)
-    setCounter(counter+1)
-    setCounter(counter+1)
+    // Since react create a batch of the same code and execute one single batch which result in updating the
+    // the value of counter only once
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+
+    // In the above process we cannot increment the value of counter by 3 however we can do this by
+    // using callback of useState
+    setCounter((prevCounter => prevCounter + 1))
+    setCounter((prevCounter => prevCounter + 1))
+    setCounter((prevCounter => prevCounter + 1))
   }
 
   const removeValue = () => {
